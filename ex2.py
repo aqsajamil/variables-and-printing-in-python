ed8ef802e3380b14bf512747f6ca3ab89aa6413b
aqsa <aqsajamil476@gmail.com>
2018-08-07 08:53:36 -0700

    variables and printing

---
+++ ex2.py
@@ -0,0 +1,36 @@
+#variable and names
+students = 100
+books = 8
+total_requires_books = students * books
+teachers = 7
+class_room = 6
+class_room_capacity = 18
+
+
+total_class_room_students_sitting = class_room * class_room_capacity
+#print total books that are required for all students
+print("total required books:",total_requires_books)
+
+print("total students sitting in all class rooms:",total_class_room_students_sitting)
+
+
+
+#more variables and printing..........
+my_name = 'AQSA JAMIL'
+my_age = 22 # not a lie
+my_eyes = 'dark brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+print ("Let's talk about %s." % my_name)
+print("my age is %d:" % my_age)
+
+print("if I add %s and %d I get it" % (my_name,my_age))
+
+
+
+
+
+
+
+
